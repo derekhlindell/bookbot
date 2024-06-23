cd4bb8468a5ac40d803515e423721d714f27159d
derek <my_email@email.com>
2024-06-23 15:02:04 -0700

    added in character counting function

--- main.py
+++ main.py
@@ -6,12 +6,26 @@ def get_num_of_words(text):
     words = len(text.split())
     return words
 
+def get_num_of_char(path):
+    text = get_book_text(path).lower()
+    char_count = {}
+
+    for char in text:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+
+    return char_count
+
 def main():
     book_path = "books/frankenstein.txt"
     book_name = book_path.split("/")[1]
     text = get_book_text(book_path)
     num_words = get_num_of_words(text)
     print(f"{num_words} words found in {book_name}")
+    char_count = get_num_of_char(book_path)
+    print(f"Here's a dictionary with a count per character: {char_count}")
 
 if __name__ == "__main__":
     main()
