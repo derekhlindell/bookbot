17e2fcfe97507facebc2d78e444e885eb98d10be
derek <my_email@email.com>
2024-06-23 15:30:03 -0700

    renamed functions to say count instead of num, added in new functions for sorting and reporting on the book

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_num_of_words(text):
+
+def get_count_of_words(text):
     words = len(text.split())
     return words
 
-def get_num_of_char(text):
+
+def get_count_of_char(text):
     char_count = {}
     for char in text.lower():
         if char not in char_count:
@@ -15,14 +17,41 @@ def get_num_of_char(text):
             char_count[char] += 1
     return char_count
 
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def sort_chars_by_count(char_dict):
+    sorted = [] 
+    for char in char_dict:
+        if not char.isalpha():
+            continue 
+        count = char_dict.get(char)
+        sorted.append({"char": char, "count": count})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
+
+
+def print_report(num_of_words, path_to_book, char_list):
+    print(f"--- Here's some info about {path_to_book} ---")
+    book_name = path_to_book.split("/")[1]
+    print(f"There are {num_of_words} words in {book_name}!\n")
+    for char_dict in char_list:
+        current_char = char_dict.get("char")
+        char_count = char_dict.get("count")
+        print(f"The character '{current_char}' appears {char_count} times")
+    print("--- End of info ---")
+
+
 def main():
     book_path = "books/frankenstein.txt"
-    book_name = book_path.split("/")[1]
     text = get_book_text(book_path)
-    num_words = get_num_of_words(text)
-    print(f"{num_words} words found in {book_name}")
-    char_count = get_num_of_char(text)
-    print(f"Here's a dictionary with a count per character: {char_count}")
+    word_count = get_count_of_words(text)
+    count_of_char = get_count_of_char(text)
+    sorted_chars = sort_chars_by_count(count_of_char)
+    print_report(word_count, book_path, sorted_chars)
+
 
 if __name__ == "__main__":
     main()
