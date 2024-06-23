30e1ff157cdda85c042a1b7ca326187addcde75f
derek <my_email@email.com>
2024-06-23 15:04:55 -0700

    reformatted get_num_of_char

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ def get_num_of_words(text):
     words = len(text.split())
     return words
 
-def get_num_of_char(path):
-    text = get_book_text(path).lower()
+def get_num_of_char(text):
     char_count = {}
-
-    for char in text:
+    for char in text.lower():
         if char not in char_count:
             char_count[char] = 1
         else:
             char_count[char] += 1
-
     return char_count
 
 def main():
@@ -24,7 +21,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_of_words(text)
     print(f"{num_words} words found in {book_name}")
-    char_count = get_num_of_char(book_path)
+    char_count = get_num_of_char(text)
     print(f"Here's a dictionary with a count per character: {char_count}")
 
 if __name__ == "__main__":
